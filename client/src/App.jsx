c8124b5b3f7933f83f4813814e72f338caa7ac52
elhemriilhame-ops <elhemriilhame@gmail.com>
2026-02-16 02:17:52 +0000

    styling simple css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react"
 import axios from 'axios'
+import "./App.css";
+
 
 function App(){
 
-const[formData , setformData] = useState({
+const[formData ,setFormData] = useState({
   firstName :'' ,
   lastName :'',
   email :'',
@@ -17,67 +19,103 @@ const[formData , setformData] = useState({
 
   const handleSubmit = async (e) =>{
     e.preventDefault()
+   //Empty fields validation
+    if (!formData.firstName ||!formData.lastName ||!formData.email ||!formData.password ||!formData.birthdate
+) {
+  alert("All fields are required!");
+  return; 
+}
+      // Email validation
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(formData.email)) {
+    alert("Invalid email format!");
+    return;
+  }
+
+  // Password validation
+  if (formData.password.length < 6) {
+    alert("Password must be at least 6 characters!");
+    return;
+  }
+
+    // connect register form to backend API
     try {
       const res = await axios.post("http://localhost:5000/user",formData)      
       console.log('Server response:', res.data);
       alert('Data posted successfully!');
+
+      
+    //  Handle API response
+     console.log(response.data);
+     alert(response.data.message);
+
       // Reset form
       setFormData({ firstName: '',lastName:'',email:'', password:'',birthdate:'' });
+
     } catch (error) {
       console.error(error)
     }
   }
-    return(
-     <>
+  return (
+  <div className="container">
+    <form className="form" onSubmit={handleSubmit}>
 
-    <form onSubmit={handleSubmit} >
-    
-      <label>First Name:</label>
-      <input
-        type="text"
-        id="firstName"
-        name="firstName"
-        value={formData.firstName}
-        onChange={handleChange}
-      />
+      <h2>REGISTER</h2>
 
-     <label>Last Name:</label>
-      <input
-        type="text"
-        name="lastName"
-        value={formData.lastName}
-        onChange={handleChange}
-      />
+      <div className="form-group">
+        <label>First Name</label>
+        <input
+          type="text"
+          name="firstName"
+          value={formData.firstName}
+          onChange={handleChange}
+        />
+      </div>
 
-     <label>Email:</label>
-      <input
-        type="text"
-        name="email"
-        value={formData.email}
-        onChange={handleChange}
-      />
+      <div className="form-group">
+        <label>Last Name</label>
+        <input
+          type="text"
+          name="lastName"
+          value={formData.lastName}
+          onChange={handleChange}
+        />
+      </div>
 
-     <label >password:</label>
-      <input
-        type="password"
-        name="password"
-        value={formData.password}
-        onChange={handleChange}
-      />
-    
-    <label> birthdate :</label>
-      <input
-        type=""
-        name="birthdate"
-        value={formData.birthdate}
-        onChange={handleChange}
-      />
-    
+      <div className="form-group">
+        <label>Email</label>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </div>
 
-    <button type="submit">create account</button>
-  </form>
-</>
- )
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </div>
 
+      <div className="form-group">
+        <label>Birthdate</label>
+        <input
+          type="date"
+          name="birthdate"
+          value={formData.birthdate}
+          onChange={handleChange}
+        />
+      </div>
+
+      <button type="submit">Create Account</button>
+
+    </form>
+  </div>
+);
 }
 export default App
